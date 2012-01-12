edebad9172f1ba61c61fcafb6171b82fc3338dda
tomek-rusilko <tomek.rusilko@gmail.com>
2012-01-13 00:32:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniaturyKatalog::Application.config.secret_token = 'fab7b2983e12b2b1c98f60e53411ae2214945093711cd8fcaf6bb16557e26c90e44e273d61ae7af0390cd4e13d6f32ab65e55f6300a9f5ca7543a7f6bf6256e3'
